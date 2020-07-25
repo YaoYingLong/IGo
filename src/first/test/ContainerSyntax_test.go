58c78a49494bf423e5881b584ada3a2a09afe059
e <>
2020-07-25 19:09:58 +0800

    补充结构体和容器示例代码

--- src/first/test/ContainerSyntax_test.go
+++ src/first/test/ContainerSyntax_test.go
@@ -9,3 +9,87 @@ func TestArrary(t *testing.T) {
 	array := [5]int{1: 10, 3: 30}
 	fmt.Println("array:", array)
 }
+
+func TestSlice(t *testing.T) {
+	{
+		var slice []int
+		fmt.Println("slice == nil:", slice == nil, len(slice), cap(slice))
+		slice = append(slice, 1)
+		fmt.Println("slice:", slice)
+		fmt.Println("slice == nil:", slice == nil, len(slice), cap(slice))
+	}
+	{
+		fmt.Println()
+		slice := []int{}
+		fmt.Println("slice == nil:", slice == nil, len(slice), cap(slice))
+		slice = append(slice, 1)
+		fmt.Println("slice:", slice)
+		fmt.Println("slice == nil:", slice == nil, len(slice), cap(slice))
+	}
+	{
+		fmt.Println()
+		var arr = [...]int{1, 2, 3, 4, 5}
+		{
+			slice := arr[:]
+			fmt.Println("slice len5 cap5:", slice, len(slice), cap(slice))
+		}
+		{
+			slice := arr[1:3]
+			fmt.Println("slice len2 cap4:", slice, len(slice), cap(slice))
+		}
+		{
+			slice := arr[1:]
+			fmt.Println("slice len4 cap4:", slice, len(slice), cap(slice))
+		}
+		{
+			slice := arr[:3]
+			fmt.Println("slice len3 cap5:", slice, len(slice), cap(slice))
+		}
+	}
+	{
+		fmt.Println()
+		var arr = [...]int{1, 2, 3, 4, 5}
+		slice := arr[1:3]
+
+		fmt.Println("slice[0]:", slice[0])
+		fmt.Println("slice[1]:", slice[1])
+		slice[0] = 12
+		fmt.Println("arr:", arr)
+
+		slice = append(slice, 13)
+		slice[0] = 14
+		fmt.Println("arr1:", arr)
+	}
+	{
+		fmt.Println()
+		var arr = [...]int{1, 2, 3, 4, 5}
+		slice := arr[1:3:3]
+		fmt.Println("slice len2 cap2:", slice, len(slice), cap(slice))
+
+		fmt.Println("slice[0]:", slice[0])
+		fmt.Println("slice[1]:", slice[1])
+		slice[0] = 12
+		fmt.Println("arr:", arr)
+
+		slice = append(slice, 13)
+		slice[0] = 14
+		fmt.Println("slice:", slice)
+		fmt.Println("arr1:", arr)
+	}
+	{
+		fmt.Println()
+		var arr = []int{1, 2, 3, 4, 5}
+		slice := arr[1:3:3]
+		fmt.Println("slice len2 cap2:", slice, len(slice), cap(slice))
+
+		fmt.Println("slice[0]:", slice[0])
+		fmt.Println("slice[1]:", slice[1])
+		slice[0] = 12
+		fmt.Println("arr:", arr)
+
+		slice = append(slice, 13)
+		slice[0] = 14
+		fmt.Println("slice:", slice)
+		fmt.Println("arr1:", arr)
+	}
+}
--- src/first/test/StructSyntax_test.go
+++ src/first/test/StructSyntax_test.go
@@ -44,3 +44,93 @@ func TestStructTag(t *testing.T) {
 		fmt.Println(catType.Tag.Get("key1"))
 	}
 }
+
+type cusUser struct {
+	name  string
+	email string
+	addr  []string
+}
+
+func (u cusUser) notifyV1() {
+	fmt.Println("V1 Sending User Email To ", u.name, u.email, u.addr)
+}
+
+func (u *cusUser) notifyV2() {
+	fmt.Println("V2 Sending User Email To ", u.name, u.email, u.addr)
+}
+
+func (u *cusUser) changeEmailV1(email string) {
+	u.email = email
+}
+
+func (u cusUser) changeEmailV2(email string) {
+	u.email = email
+}
+
+func (u *cusUser) changeAddrV11(addr []string, addAddr string) {
+	u.addr = append(addr, addAddr)
+}
+
+func (u *cusUser) changeAddrV12(addAddr string) {
+	u.addr = append(u.addr, addAddr)
+}
+
+func (u *cusUser) changeAddrV13(addr []string, addAddr string) {
+	addr = append(addr, addAddr)
+}
+
+func (u cusUser) changeAddrV21(addr []string, addAddr string) {
+	u.addr = append(addr, addAddr)
+}
+
+func (u cusUser) changeAddrV22(addAddr string) {
+	u.addr = append(u.addr, addAddr)
+}
+
+func (u cusUser) changeAddrV23(addr []string, addAddr string) {
+	addr = append(addr, addAddr)
+}
+
+func TestMethod(t *testing.T) {
+	bill := cusUser{"bill", "bill@email.com", []string{"a", "b"}}
+	bill.notifyV1()
+	bill.notifyV2()
+
+	bill.changeEmailV1("bill@emailv1.com")
+	bill.notifyV1()
+	bill.notifyV2()
+
+	bill.changeEmailV2("bill@emailv2.com")
+	bill.notifyV1()
+	bill.notifyV2()
+
+	bill.changeAddrV11(bill.addr, "c")
+	bill.notifyV1()
+
+	bill.changeAddrV12("d")
+	bill.notifyV1()
+
+	bill.changeAddrV13(bill.addr, "e")
+	bill.notifyV1()
+
+	bill.changeAddrV21(bill.addr, "f")
+	bill.notifyV1()
+
+	bill.changeAddrV22("g")
+	bill.notifyV1()
+
+	bill.changeAddrV23(bill.addr, "h")
+	bill.notifyV1()
+
+	lisa := &cusUser{"lisa", "lisa@emial.com", []string{"a", "b"}}
+	lisa.notifyV1()
+	lisa.notifyV2()
+
+	lisa.changeEmailV1("lisa@emailv1.com")
+	lisa.notifyV1()
+	lisa.notifyV2()
+
+	lisa.changeEmailV2("lisa@emailv2.com")
+	lisa.notifyV1()
+	lisa.notifyV2()
+}
